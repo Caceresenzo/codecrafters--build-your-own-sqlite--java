156a2313a17d0f613dcde6bba4ea851e84cc28bb
Caceresenzo <caceresenzo1502@gmail.com>
2024-01-05 00:03:42 +0100

    pass 8th stage

--- src/main/java/sqlite/SQLiteParser.java
+++ src/main/java/sqlite/SQLiteParser.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import sqlite.domain.Cell;
 import sqlite.domain.Database;
